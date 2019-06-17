be53e7b5db23e9b99b669692d342dc40de9fc843
Sinan Gunes <sinan@medici.md>
2019-06-17 15:13:36 +0300

    Using behaviour subject instead of publish subject, release 2.2.2

--- library/src/main/java/com/petarmarijanovic/rxactivityresult/ActivityResult.java
+++ library/src/main/java/com/petarmarijanovic/rxactivityresult/ActivityResult.java
@@ -3,56 +3,58 @@ package com.petarmarijanovic.rxactivityresult;
 import android.app.Activity;
 import android.content.Intent;
 
-/** Created by petar on 26/07/2017. */
+/**
+ * Created by petar on 26/07/2017.
+ */
 public class ActivityResult {
-  private int resultCode;
-  private Intent data;
+    private int resultCode;
+    private Intent data;
 
-  public ActivityResult(int resultCode, Intent data) {
-    this.resultCode = resultCode;
-    this.data = data;
-  }
+    public ActivityResult(int resultCode, Intent data) {
+        this.resultCode = resultCode;
+        this.data = data;
+    }
 
-  public int getResultCode() {
-    return resultCode;
-  }
+    public int getResultCode() {
+        return resultCode;
+    }
 
-  public Intent getData() {
-    return data;
-  }
+    public Intent getData() {
+        return data;
+    }
 
-  public boolean isOk() {
-    return resultCode == Activity.RESULT_OK;
-  }
+    public boolean isOk() {
+        return resultCode == Activity.RESULT_OK;
+    }
 
-  public boolean isCanceled() {
-    return resultCode == Activity.RESULT_CANCELED;
-  }
+    public boolean isCanceled() {
+        return resultCode == Activity.RESULT_CANCELED;
+    }
 
-  public boolean isFirstUser() {
-    return resultCode == Activity.RESULT_FIRST_USER;
-  }
+    public boolean isFirstUser() {
+        return resultCode == Activity.RESULT_FIRST_USER;
+    }
 
-  @Override
-  public int hashCode() {
-    int result = resultCode;
-    result = 31 * result + (data != null ? data.hashCode() : 0);
-    return result;
-  }
+    @Override
+    public int hashCode() {
+        int result = resultCode;
+        result = 31 * result + (data != null ? data.hashCode() : 0);
+        return result;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (!(o instanceof ActivityResult)) return false;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ActivityResult)) return false;
 
-    ActivityResult that = (ActivityResult) o;
+        ActivityResult that = (ActivityResult) o;
 
-    if (resultCode != that.resultCode) return false;
-    return data != null ? data.equals(that.data) : that.data == null;
-  }
+        if (resultCode != that.resultCode) return false;
+        return data != null ? data.equals(that.data) : that.data == null;
+    }
 
-  @Override
-  public String toString() {
-    return "ActivityResult{" + "resultCode=" + resultCode + ", data=" + data + '}';
-  }
+    @Override
+    public String toString() {
+        return "ActivityResult{" + "resultCode=" + resultCode + ", data=" + data + '}';
+    }
 }
